e2ebc7a6310a8408c0e727711fd5d9f776376101
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-13 22:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -27,19 +27,6 @@ class Teacher extends Person {
 	}
 }
 
-
-const person = new Person("John", 25);
-console.log(person._name);
-person.age = 30;
-
-const student = new Student("John", 30);
-console.log(student._name);
-student.study();
-
-const teacher = new Teacher("Bob", 35);
-console.log(teacher._name);
-teacher.teach();
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
